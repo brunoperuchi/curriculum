b6d3cc31031600d6e8e0d1a817f24f152ab6d600
Bruno Peruchi <peruchib@gmail.com>
2022-05-17 21:17:41 -0300

    fixed header and created home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header/Index";
+import { Home } from "./components/Home";
 import { Skills } from "./components/Skills";
 
 export function App() {
   return (
     <>
       <Header />
+      <Home />
       <Skills />
     </>
   );
--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -3,9 +3,7 @@ import { NavHeader } from "./Content/NavHeader";
 
 export function Header() {
   return (
-    <header
-    className="bg-zinc-800 text-zinc-100 flex flex-col sm:flex-row items-center sm:justify-around w-full py-1"
-    >
+    <header className="fixed z-30 bg-zinc-800 text-zinc-100 flex flex-col sm:flex-row items-center sm:justify-around w-full py-1">
       <InfoHeader />
       <NavHeader />
     </header>
