6f9e530e3758701c751d02c3f764d01ede6b820c
Bruno Peruchi <peruchib@gmail.com>
2022-05-29 11:47:18 -0300

    adjusted Footer spacing and centering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="w-full h-8 mt-4 bg-zinc-800 flex items-center justify-center border-t-2 border-purple-800">
-      <p className="text-sm text-zinc-500">
+    <footer className="w-full mt-4 bg-zinc-800 flex items-center justify-center border-t-2 border-purple-800">
+      <p className="m-2 text-xs text-zinc-500 text-center">
         Thank you for taking a look at my work, and a warm hug! <strong className="text-purple-800 font-normal">;)</strong>
       </p>
     </footer>
